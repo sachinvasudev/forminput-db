a1fde602f0dd8f403ab1470856f2ff097bd98ffd
Sachin Vasudev <sachinvasudev@gmail.com>
2011-09-25 22:50:57 +0530

    minor changed to db structure such as making id auto_increment

--- form.sql
+++ form.sql
@@ -3,14 +3,14 @@
 --
 
 CREATE TABLE IF NOT EXISTS `employee` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
-  `age` int(11) NOT NULL,
+  `age` int(2) NOT NULL,
   `occupation` varchar(20) NOT NULL,
   `address` varchar(200) NOT NULL,
   `status` enum('active','inactive') NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Dumping data for table `employee`
